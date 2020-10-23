5115d70b4dbcfc8022acca215e0c4b84402dcfa0
Dary <daryalexw@gmail.com>
2020-10-23 20:56:20 +0300

    Homework_lesson1

---
+++ MazeGameMVC/Assets/Scripts/CameraStruct.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+
+
+namespace MazeGame
+{
+    public class CameraStruct : MonoBehaviour
+    {
+        public Vector3 offset; //= Vector2.zero;
+        public Transform target;
+    }
+}
